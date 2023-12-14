0f71871a92e9dcc66252ae312db2e23f6d739294
User <User@DESKTOP-3G79H5S>
2023-12-14 14:51:54 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      int [] nums = { 2, 7, 11, 15 };
+      int target = 9;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j]== target)
+                {
+        
+                    Console.WriteLine(nums[i] + nums[j]);
+                }
+            }
+        }
+    }
+}
